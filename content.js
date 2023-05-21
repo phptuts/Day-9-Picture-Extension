36027f2b7dda64378688bf751fb9651a843c657b
phptuts <glaserpower@gmail.com>
2023-05-21 09:10:30 -0700

    video-4-communication

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-
+chrome.runtime.onMessage.addListener((message) => {
+  console.log(message);
+  const imgs = document.getElementsByTagName("img");
+  for (var img of imgs) {
+    console.log(img);
+  }
+});
