a0eab84656df69ee897065cb165d3bd919b717b2
phptuts <glaserpower@gmail.com>
2023-05-20 22:04:28 -0700

    2-options-page

--- options.js
+++ options.js
@@ -1,1 +1,16 @@
+const buttonEl = document.querySelector("button");
+const imageUrlEl = document.getElementById("image");
+const imageReplaceEl = document.getElementById("replace_image");
 
+buttonEl.addEventListener("click", () => {
+  const url = imageUrlEl.value;
+  chrome.storage.sync.set({ url }, function () {
+    imageReplaceEl.src = url;
+  });
+});
+
+chrome.storage.sync.get("url", (data) => {
+  if (data.url) {
+    imageReplaceEl.src = data.url;
+  }
+});
